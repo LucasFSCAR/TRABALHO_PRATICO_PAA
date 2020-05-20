539352455d5862980513ecc61d698ef27da6d880
Pedro Egg <pedro.egg@sga.pucminas.br>
2020-05-20 00:54:37 -0300

    Adicionando método de leitura de entrada

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#include <sstream>
+#include <string>
 #include <vector>
 #include "cidade.hpp"
 #include "caixeiroViajante.hpp"
 
 using namespace std;
+
+void lerEntrada();
 
 int main()
 {
@@ -22,8 +26,38 @@ int main()
     lista.push_back(cidade2);
     lista.push_back(cidade3);
     //lista.push_back(cidade4);
+    
+    //lerEntrada();
 
     CaixeiroViajante cv(lista);
     cv.caixeiroViajanteForcaBruta();
+
     return 0;
+}
+
+void lerEntrada() {
+    string entrada1, entrada2;
+    bool read = false;
+
+    for (string line; getline(cin, line);) {
+        if(read == false){
+            read = true;
+            continue;
+        }
+
+        vector<string> array;
+
+        istringstream split(line);
+
+        for(string temp; split >> temp;){
+            array.push_back(temp);
+        }
+
+        entrada1 = array[0];
+        entrada2 = array[1];
+        
+        //cout << entrada1 << " " << entrada2 << endl;
+        
+        //FAZER O QUE FOR NECESSARIO
+    }
 }
