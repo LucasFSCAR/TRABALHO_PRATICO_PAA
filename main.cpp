c80948ed9d2925e5cbed9667bcc8c4e925e2c411
brunorgs <brunogonzaga10@hotmail.com>
2020-05-24 15:56:12 -0300

    ajuste print

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ using namespace std;
 void lerEntrada(vector<Cidade> &lista);
 
 int main()
-{   
+{
     // Declaracao lista de cidades
     vector<Cidade> lista;
 
@@ -25,39 +25,48 @@ int main()
     CaixeiroViajante cv(lista);
 
     // Executamos as funcoes do caixeiro viajante e salvamos os tempos para cada execucao
+    cout << "\nForça Bruta \n";
+
     clock_t inicio1 = clock();
     cv.caixeiroViajanteForcaBruta();
     clock_t fim1 = clock();
+
+    double tempoDecorrido1 = double(fim1 - inicio1) / CLOCKS_PER_SEC;
+    cout << "Tempo: " << tempoDecorrido1 << "\n\n";
+
+    cout << "Dinamico \n";
 
     clock_t inicio2 = clock();
     cv.caixeiroViajanteDinamico();
     clock_t fim2 = clock();
 
+    double tempoDecorrido2 = double(fim2 - inicio2) / CLOCKS_PER_SEC;
+    cout << "Tempo: " << tempoDecorrido2 << "\n\n";
+
+    cout << "Branch And Bound \n";
+
     clock_t inicio3 = clock();
     cv.caixeiroViajanteBranchAndBound();
     clock_t fim3 = clock();
+
+    double tempoDecorrido3 = double(fim3 - inicio3) / CLOCKS_PER_SEC;
+    cout << "Tempo: " << tempoDecorrido3 << "\n\n";
+
+    cout << "Genetico \n";
 
     clock_t inicio4 = clock();
     cv.caixeiroViajanteGenetico();
     clock_t fim4 = clock();
 
-    double tempoDecorrido1 = double(fim1 - inicio1) / CLOCKS_PER_SEC;
-    double tempoDecorrido2 = double(fim2 - inicio2) / CLOCKS_PER_SEC;
-    double tempoDecorrido3 = double(fim3 - inicio3) / CLOCKS_PER_SEC;
     double tempoDecorrido4 = double(fim4 - inicio4) / CLOCKS_PER_SEC;
-
-    // Print dos tempos para cada execucao
-    cout << "\nForça Bruta: " << tempoDecorrido1 << "\n";
-    cout << "Dinamico: " << tempoDecorrido2 << "\n";
-    cout << "Branch And Bound: " << tempoDecorrido3 << "\n";
-    cout << "Genetico: " << tempoDecorrido4 << "\n";
+    cout << "Tempo: " << tempoDecorrido4 << "\n";
 
     return 0;
 }
 
 // Metodo que le a entrada
 void lerEntrada(vector<Cidade> &lista)
-{   
+{
     // Declaracao variaveis
     int coordenadaX, coordenadaY;
     bool read = false;
@@ -65,7 +74,7 @@ void lerEntrada(vector<Cidade> &lista)
 
     // Para cada linha da entrada
     for (string line; getline(cin, line);)
-    {   
+    {
         // Pulando a primeira linha da entrada (linha que indica o numero de linhas)
         if (read == false)
         {
