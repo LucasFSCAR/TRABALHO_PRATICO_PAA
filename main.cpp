7ac5cdc9e39b4b5c1b651560cf4715972806e6aa
Lucas <lucas123carvalho@icloud.com>
2020-05-23 23:10:17 -0300

    Fim²

--- main.cpp
+++ main.cpp
@@ -19,34 +19,6 @@ int main()
 
     cout << "Started\n";
 
-    /*
-    Cidade cidade0(1, 100, 100);
-    Cidade cidade1(2, 900, 100);
-    Cidade cidade2(3, 900, 900);
-    Cidade cidade3(4, 100, 900);
-    Cidade cidade4(5, 500, 500);
-    Cidade cidade5(1, 100, 100);
-    Cidade cidade6(2, 900, 100);
-    Cidade cidade7(3, 900, 900);
-    Cidade cidade8(4, 100, 900);
-    Cidade cidade9(5, 500, 500);
-    //Cidade cidade10(1, 1000, 1000);
-    //Cidade cidade11(2, 900, 100);
-
-    lista.push_back(cidade0);
-    lista.push_back(cidade1);
-    lista.push_back(cidade2);
-    lista.push_back(cidade3);
-    lista.push_back(cidade4);
-    lista.push_back(cidade5);
-    lista.push_back(cidade6);
-    lista.push_back(cidade7);
-    lista.push_back(cidade8);
-    lista.push_back(cidade9);
-    //lista.push_back(cidade10);
-    //lista.push_back(cidade11);
-    */
-
     lerEntrada(lista);
 
     // Instanciamos um objeto do tipo CaixeiroViajante com a lista de cidades
@@ -58,11 +30,11 @@ int main()
     clock_t fim1 = clock();
 
     clock_t inicio2 = clock();
-    //cv.caixeiroViajanteDinamico();
+    cv.caixeiroViajanteDinamico();
     clock_t fim2 = clock();
 
     clock_t inicio3 = clock();
-    //cv.caixeiroViajanteBranchAndBound();
+    cv.caixeiroViajanteBranchAndBound();
     clock_t fim3 = clock();
 
     clock_t inicio4 = clock();
